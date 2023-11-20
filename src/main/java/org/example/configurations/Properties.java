0a15311de9d4751b3bb7be9c37cdb91976b007b1
PatesM <maximpates@gmail.com>
2023-11-20 09:34:58 +0500

    task 7.2.3 - change encoding.

--- src/main/java/org/example/configurations/Properties.java
+++ src/main/java/org/example/configurations/Properties.java
@@ -3,5 +3,6 @@ package org.example.configurations;
 public class Properties {
 
     public static final String WB_MAIN_PAGE_URL = "https://www.wildberries.ru/";
-    public static final String SEARCH_VALUE = "мобильный телефон";
+    public static final String HEROKUAPP_PAGE_URL = "https://www.wildberries.ru/";
+    public static final String SEARCH_VALUE = "РјРѕР±РёР»СЊРЅС‹Р№ С‚РµР»РµС„РѕРЅ";
 }
